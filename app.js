877d3aeca64d13d76377b34f0e2d791f944be19c
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-12-03 23:58:57 -0500

    Formatted styling for document

--- app.js
+++ app.js
@@ -13,118 +13,100 @@ const render = require("./lib/htmlRenderer");
 let employees = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-const  addEmployee = () => {inquirer.prompt([
-    {
-        type: 'list',
-        name: 'title',
-        message: 'Which of the following describes your job title?',
-        choices: ["Manager", "Engineer","Intern"]
-    },
-
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?',
-    },
-    {
-        type: "input",
-        name: "id",
-        message:'What is your ID number?',
-    },
-    {
-        type: "input",
-        name: "officeNumber",
-        message:'What is your office number?',
-        when: function( answers ) {
-          return answers.title === "Manager";
-     }
-    },
-    {
-        type: "input",
-        name: "github",
-        message:'What is your GitHub username?',
-        when: function( answers ) {
-          return answers.title === "Engineer";
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'title',
+            message: 'Which of the following describes your job title?',
+            choices: ["Manager", "Engineer", "Intern"]
         },
 
-    },
-    {
-        type: "input",
-        name: "school",
-        message:'What is the name of your school?',
-        when: function( answers ) {
-          return answers.title === "Intern";
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
         },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: "input",
+            name: "id",
+            message: 'What is your ID number?',
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: 'What is your office number?',
+            when: function (answers) {
+                return answers.title === "Manager";
+            }
+        },
+        {
+            type: "input",
+            name: "github",
+            message: 'What is your GitHub username?',
+            when: function (answers) {
+                return answers.title === "Engineer";
+            },
 
-    },
-    { 
-        type: 'list',
-        message: "Would you like to add another employee?",
-        name: 'addEmployee',
-        choices: ['yes', 'no']
-    } 
-    
- ])
- .then((data) => {
-   console.log(data); 
+        },
+        {
+            type: "input",
+            name: "school",
+            message: 'What is the name of your school?',
+            when: function (answers) {
+                return answers.title === "Intern";
+            },
 
-   // Check the employee type
-   switch(data.title){
-       // Constuct the class for that type (Manager, Eng, Intern)
-       // Add constructed employee class to team array
-       case 'Manager': 
-       const newManager = new Manager(data.name, data.id, data.email, data.officeNumber);
-       employees.push(newManager);
-       break;
+        },
+        {
+            type: 'list',
+            message: "Would you like to add another employee?",
+            name: 'addEmployee',
+            choices: ['yes', 'no']
+        }
 
-       case 'Engineer': 
-       const newEngineer = new Engineer(data.name, data.id, data.email, data.github);
-       employees.push(newEngineer);
-       break;
+    ])
+    .then((data) => {
 
-       case 'Intern': 
-       const newIntern = new Intern(data.name, data.id, data.email, data.school);
-       employees.push(newIntern);
-       break;
-   }
-   
-   // Check the value of the 'add employee' question.
-   // if yes, run questions again
-   if(data.addEmployee === 'yes'){
-    addEmployee(); 
-   }
-   else{
-       renderTeam();
-   }
-});
+        // Check the employee type
+        switch (data.title) {
+            // Constuct the class for that type (Manager, Eng, Intern)
+            // Add constructed employee class to team array
+            case 'Manager':
+                const newManager = new Manager(data.name, data.id, data.email, data.officeNumber);
+                employees.push(newManager);
+                break;
+
+            case 'Engineer':
+                const newEngineer = new Engineer(data.name, data.id, data.email, data.github);
+                employees.push(newEngineer);
+                break;
+
+            case 'Intern':
+                const newIntern = new Intern(data.name, data.id, data.email, data.school);
+                employees.push(newIntern);
+                break;
+        }
+
+        // Check the value of the 'add employee' question.
+        // if yes, run questions again
+        if (data.addEmployee === 'yes') {
+            addEmployee();
+        }
+        else {
+            renderTeam();
+        }
+    });
 }
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 
 const renderTeam = () => {
     const team = render(employees);
-    fs.writeFile(outputPath, team, (err) => 
-    err ? console.log(err) : console.log("Success!"))
- }
- addEmployee();
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! 
+    fs.writeFile(outputPath, team, (err) =>
+    err ? console.log(err) : console.log("Success!"));
+}
+addEmployee();
